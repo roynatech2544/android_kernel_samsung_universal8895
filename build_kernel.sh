6415e1acf4c9a13309cc28e3d6e80492a82870e4
roynatech2544 <whiteshell2544@naver.com>
2023-05-20 23:07:41 +0000

    build_kernel: Switch to LLVM=1

--- build_kernel.sh
+++ build_kernel.sh
@@ -7,5 +7,5 @@ export KBUILD_BUILD_HOST=GrassLand
 PATH=$PWD/toolchain/bin:$PATH
 
 rm -rf out
-make O=out ARCH=arm64 CC=clang CROSS_COMPILE=aarch64-linux-gnu- -j$(nproc) exynos8895-dreamlte_defconfig
-make O=out ARCH=arm64 CC=clang AS=llvm-as LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- -j$(nproc)
+make O=out LLVM=1 CROSS_COMPILE=aarch64-linux-gnu- -j$(nproc) exynos8895-dreamlte_defconfig
+make O=out LLVM=1 CROSS_COMPILE=aarch64-linux-gnu- -j$(nproc)
