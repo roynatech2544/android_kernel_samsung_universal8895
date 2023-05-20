96a1c1ee21bdd661c63f4843bcaf9008883abdfe
roynatech2544 <whiteshell2544@naver.com>
2023-05-20 09:42:03 +0800

    Revert "build_kernel: Cant use lld for now"
    This reverts commit 3d31751e5ad1f5b8221290bb9bccf95b12e06f2c.

--- build_kernel.sh
+++ build_kernel.sh
@@ -8,5 +8,4 @@ PATH=$PWD/toolchain/bin:$PATH
 
 rm -rf out
 make O=out ARCH=arm64 CC=clang CROSS_COMPILE=aarch64-linux-gnu- -j$(nproc) exynos8895-dreamlte_defconfig
-# FIXME: LLD needs to be used
-make O=out ARCH=arm64 CC=clang AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- -j$(nproc)
+make O=out ARCH=arm64 CC=clang AS=llvm-as LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- -j$(nproc)
